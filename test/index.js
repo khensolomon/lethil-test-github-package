7d0882ce2b7b228fe1aca998d1c4eed29ec20fef
Khen Solomon Lethil <khensolomon@gmail.com>
2021-03-28 10:59:51 +0200

    Updating

--- test/index.js
+++ test/index.js
@@ -1,1 +1,10 @@
-console.log('Ok')
+import 'mocha';
+import core from '../lib/index.js';
+import assert from 'assert';
+
+describe('Checking', () => {
+	it('Hello world', () => {
+		const job = core();
+		assert.strictEqual('Hello world',job);
+	});
+});
